6197102f869d37391ac5967b7889ad8f2286fa56
mrvladimort <mrvladimort@gmail.com>
2019-06-08 17:41:05 +0200

    version backend integration

--- src/configs/main.js
+++ src/configs/main.js
@@ -1,4 +1,6 @@
 export default {
-    clientHost: "http://127.0.0.1:3000",
-    apiHost: "http://127.0.0.1:8080/api"
+    // apiHost: "http://127.0.0.1:8080/api"
+    // clientHost: "http://127.0.0.1:3000",
+    apiHost: "https://ipb-project-app-back.herokuapp.com/api",
+    clientHost: "https://ipb-project-app-front.herokuapp.com",
 }
